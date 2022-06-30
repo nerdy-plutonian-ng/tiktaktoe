d16eb6d1d17f17209aaec6bf33debd8ff0e498fa
nerdy-plutonian-ng <nerdymykl@gmail.com>
2022-06-30 14:43:09 +0000

    added comment on getAppTheme

--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'prefs_ops.dart';
 import 'app_theme.dart';
 
+//dynamically get app theme
 Future<ThemeData> getAppTheme() async {
   final userInfo = await getUserInfo();
   return userInfo == null
